134234ef2d23145b238bd23fa0ac895b3fe6cb64
adwaithks <adwaith2507@gmail.com>
2022-04-24 22:42:36 +0530

    news population

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', fetchFakeNews);
 const newsContainer = document.querySelector('.news-container');
 
+function setCardColor(prediction) {
+    if (prediction == "Real") 
+        return "green"
+    else
+        return "red"
+}
+
 function populateNews(news) {
     let newsContent = "";
     if (news.length == 0) {
@@ -13,34 +20,38 @@ function populateNews(news) {
         return;
     }
     news.forEach((newsArticle) => {
+        let color = setCardColor(newsArticle.prediction);
         newsContent += `
-            <div class="news-article">
+            <div class="news-article ${color}">
                 <h1 class="news-title">${newsArticle.title}</h1>
                 <div class="news-article-info">
                     <div class="news-article-source-date">
                         <h5>${newsArticle.source}</h5>
-                        <h5>${newsArticle.datePosted}</h5>
+                        <h5>${newsArticle.date_posted}</h5>
                     </div>
-                    <h5>${newsArticle.prediction}</h5>
+                    <div class="news-article-prediction-container">
+                        <h5 class="news-article-prediction">${newsArticle.prediction}</h5>
+                    </div>
                 </div>
             </div>
         `
+        newsContainer.innerHTML = newsContent;
     });
 }
 
 
 function fetchFakeNews() {
     let data = [];
-    populateNews(data);
-    /*
-    let url = `https://fakenserver.herokuapp.com/api/predictednews`;
+    //populateNews(data);
+    let url = 'http://localhost:8000/api/predictednews';
+    //let url = `https://fakenserver.herokuapp.com/api/predictednews`;
     fetch(url)
     .then(res => res.json())
     .then(data => {
         populateNews(data);
     }).catch(err => {
-
+        console.log(err);
     });
-    */
+    
 }
 
